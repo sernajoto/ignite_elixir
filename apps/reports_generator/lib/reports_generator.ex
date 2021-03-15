c3ba4185776b2899143933fde212535a5954f714
Guilherme Sernajoto <guilhermesernajoto@hotmail.com>
2021-03-15 18:24:19 -0300

    feat: added reports generator app

---
+++ apps/reports_generator/lib/reports_generator.ex
@@ -0,0 +1,42 @@
+defmodule ReportsGenerator do
+  alias ReportsGenerator.Parser
+
+  @available_foods [
+    "açaí",
+    "churrasco",
+    "esfirra",
+    "hambúrguer",
+    "pastel",
+    "pizza",
+    "prato_feito",
+    "sushi"
+  ]
+
+  @options ["foods", "users"]
+
+  def build(filename) do
+    filename
+    |> Parser.parse_file()
+    |> Enum.reduce(report_acc(), fn line, report -> sum_values(line, report) end)
+  end
+
+  def fetch_higher_cost(report, option) when option in @options do
+    {:ok, Enum.max_by(report[option], fn {_key, value} -> value end)}
+  end
+
+  def fetch_higher_cost(_report, _option), do: {:error, "Invalid option!"}
+
+  defp sum_values([id, food_name, price], %{"foods" => foods, "users" => users} = report) do
+    users = Map.put(users, id, users[id] + price)
+    foods = Map.put(foods, food_name, foods[food_name] + 1)
+
+    %{report | "users" => users, "foods" => foods}
+  end
+
+  defp report_acc do
+    foods = Enum.into(@available_foods, %{}, &{&1, 0})
+    users = Enum.into(1..30, %{}, &{Integer.to_string(&1), 0})
+
+    %{"users" => users, "foods" => foods}
+  end
+end
