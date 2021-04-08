68e97f0b8d75f044e65663b52290187eed9762d3
Guilherme Sernajoto <guilhermesernajoto@hotmail.com>
2021-04-08 07:59:56 -0300

    feat(exlivery): added report generation in .csv

--- apps/exlivery/lib/orders/agent.ex
+++ apps/exlivery/lib/orders/agent.ex
@@ -16,6 +16,8 @@ defmodule Exlivery.Orders.Agent do
 
   def get(uuid), do: Agent.get(__MODULE__, &get_order(&1, uuid))
 
+  def list_all, do: Agent.get(__MODULE__, & &1)
+
   defp update_state(state, %Order{} = order, uuid), do: Map.put(state, uuid, order)
 
   defp get_order(state, uuid) do
