1814410e73cb09c590a10c23f800195087fa7bf5
Guilherme Sernajoto <guilhermesernajoto@hotmail.com>
2021-07-19 21:55:38 -0300

    feat(rockelivery): added the item migration, model, create and post route

---
+++ apps/rockelivery/lib/rockelivery/item.ex
@@ -0,0 +1,29 @@
+defmodule Rockelivery.Item do
+  use Ecto.Schema
+
+  import Ecto.Changeset
+
+  @required_params [:category, :description, :price, :photo]
+  @items_categories [:food, :drink, :desert]
+
+  @derive {Jason.Encoder, only: @required_params ++ [:id]}
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+
+  schema "items" do
+    field :category, Ecto.Enum, values: @items_categories
+    field :description, :string
+    field :price, :decimal
+    field :photo, :string
+
+    timestamps()
+  end
+
+  def changeset(struct \\ %__MODULE__{}, params) do
+    struct
+    |> cast(params, @required_params)
+    |> validate_required(@required_params)
+    |> validate_length(:description, min: 1)
+    |> validate_number(:price, greater_than: 0)
+  end
+end
