d6ef74dfd8aee17538a89df54c7bdb4a821ab70b
Guilherme Sernajoto <guilhermesernajoto@hotmail.com>
2021-08-13 22:11:02 -0300

    build(rockelivery): add deploy stage

--- apps/rockelivery/priv/repo/seeds.exs
+++ apps/rockelivery/priv/repo/seeds.exs
@@ -9,3 +9,44 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Rockelivery.{Item, Order, Repo, User}
+
+user = %User{
+  age: 24,
+  address: "Rua Teste",
+  cep: "18078344",
+  cpf: "94222345245",
+  email: "gui@teste.com",
+  password: "123456",
+  name: "Gui"
+}
+
+%User{id: user_id} = Repo.insert!(user)
+
+item1 = %Item{
+  category: :food,
+  description: "Banana frita",
+  price: Decimal.new("15.50"),
+  photo: "priv/photos/banana_frita.png"
+}
+
+item2 = %Item{
+  category: :food,
+  description: "Banana cozida",
+  price: Decimal.new("12.50"),
+  photo: "priv/photos/banana_cozida.png"
+}
+
+Repo.insert!(item1)
+Repo.insert!(item2)
+
+order = %Order{
+  user_id: user_id,
+  items: [item1, item2],
+  address: "Rua teste",
+  comments: "Sem canela",
+  payment_method: :money
+}
+
+Repo.insert!(order)
